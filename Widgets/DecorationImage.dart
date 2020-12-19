8898d6e70aca5c8a5b4e5fcfc5a576ca8bf581f7
Rajat Surana <57813120+suranasaheb99@users.noreply.github.com>
2020-12-19 22:54:35 +0530

    DecorationImage.dart

---
+++ Widgets/DecorationImage.dart
@@ -0,0 +1,21 @@
+//An image for a box decoration.
+//The image is painted using paintImage, which describes the meanings of the various fields on this class in more detail.
+
+DecorationImage({required ImageProvider<Object> image, 
+                ImageErrorListener? onError,
+                ColorFilter? colorFilter,
+                BoxFit? fit,
+                AlignmentGeometry alignment: Alignment.center,
+                Rect? centerSlice, ImageRepeat repeat: ImageRepeat.noRepeat, 
+                bool matchTextDirection: false, double scale: 1.0})
+
+//Have some basic properties like alignment-----------
+/*centerSlice
+colorFilter
+fit
+hashCode
+image
+matchTextDirection
+onError
+repeat
+*/
